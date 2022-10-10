6a10278aa5f654abf5897c4920a891d287266803
marcos <marcos.bonet@live.com.ar>
2022-10-10 16:19:36 +0200

    asignation variable

--- utils.js
+++ utils.js
@@ -1,4 +1,13 @@
 
+/*declaracion de variable
+export function bar(){
+    let x = 22*2 ;
+    if (x>12){
+    console.log(x);
+}
+};
+*/
+/* asignacion de variable*/
 export const foo = () => {
     let x = 22*2 ;
     if (x>12){
